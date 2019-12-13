8ce08e49ffebd6a157f6f2aa7deb4087401127a4
DjukicBogdan <bogdandj264@gmail.com>
2019-12-13 15:23:57 +0100

    Initial commit

---
+++ Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPhoneBook.Models
+{
+    [Table("Person")]
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
